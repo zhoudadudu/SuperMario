b582641fd637afd0e90ca23bc961f374fc70712f
zhoudadudu <72678330+zhoudadudu@users.noreply.github.com>
2022-11-12 20:09:28 +0800

    Add files via upload
    第28集蘑菇变大完成！

--- SuperMario/source/components/powerup.py
+++ SuperMario/source/components/powerup.py
@@ -21,11 +21,70 @@ class Powerup(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = centerx
         self.rect.centery = centery
+        self.origin_y = centery - self.rect.height/2
+
+        self.x_vel = 0
+        self.direction = 1  # facing right
+        self.y_vel = -1
+        self.gravity = 1
+        self.max_y_vel = 8
+
+    def update_position(self, level):
+        self.rect.x += self.x_vel
+        self.check_x_collision(level)
+        self.rect.y += self.y_vel
+        self.check_y_collision(level)
+
+        if self.rect.x < 0 or self.rect.y > C.SCREEN_H:
+            self.kill()
+
+    def check_x_collision(self, level):
+        sprite = pygame.sprite.spritecollideany(self, level.ground_items_group)
+        if sprite:
+            #self.direction = 1 if self.direction == 0 else 0
+            if self.direction:
+                self.direction = 0
+                self.rect.right = sprite.rect.left
+            else:
+                self.direction = 1
+                self.rect.left = sprite.rect.right
+            self.x_vel *= -1
+
+
+    def check_y_collision(self, level):
+        check_group = pygame.sprite.Group(level.ground_items_group, level.box_group, level.brick_group)
+        sprite = pygame.sprite.spritecollideany(self, check_group)
+        if sprite:
+            if self.rect.top < sprite.rect.top:
+                self.rect.bottom = sprite.rect.top
+                self.y_vel = 0
+                self.state = 'walk'
+
+        level.check_will_fall(self)
 
 
 class Mushroom(Powerup):
     def __init__(self, centerx, centery):
         Powerup.__init__(self, centerx, centery, [(0, 0, 16, 16)])
+        self.x_vel = 2
+        self.state = 'grow'
+        self.name = 'mushroom'
+
+    def update(self, level):
+        if self.state == 'grow':
+            self.rect.y += self.y_vel
+            if self.rect.bottom < self.origin_y:
+                self.state = 'walk'
+        elif self.state == 'walk':
+            pass
+        elif self.state == 'fall':
+            if self.y_vel < self.max_y_vel:
+                self.y_vel += self.gravity
+
+        if self.state != 'grow':
+            self.update_position(level)
+
+
 
 class Fireball(Powerup):
     pass
